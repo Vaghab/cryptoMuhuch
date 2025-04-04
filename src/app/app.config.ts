f1ea06a9639cda2aaeaf1c34c9973f62504d1fc6
Vaghab <131035233+Vaghab@users.noreply.github.com>
2025-04-04 17:18:13 +0300

    feat: настройка конфига

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,8 +1,34 @@
-import { ApplicationConfig } from '@angular/core';
+import { provideAnimations } from '@angular/platform-browser/animations';
+import { HttpClient, provideHttpClient, withFetch } from '@angular/common/http';
+import { ApplicationConfig, importProvidersFrom } from '@angular/core';
+import { provideClientHydration } from '@angular/platform-browser';
 import { provideRouter } from '@angular/router';
+import { TranslateLoader, TranslateModule } from '@ngx-translate/core';
 
+import { HttpLoaderFactory } from '../translate';
 import { routes } from './app.routes';
+import { DOCUMENT } from '@angular/common';
+import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideRouter(routes)]
+  providers: [
+    provideAnimations(),
+    provideRouter(routes),
+    provideClientHydration(),
+    {
+      provide: Document,
+      useExisting: DOCUMENT,
+    },
+    provideHttpClient(withFetch()),
+    importProvidersFrom([
+      TranslateModule.forRoot({
+        loader: {
+          provide: TranslateLoader,
+          useFactory: HttpLoaderFactory,
+          deps: [HttpClient],
+        },
+      }),
+    ]),
+    provideAnimationsAsync(),
+  ],
 };
